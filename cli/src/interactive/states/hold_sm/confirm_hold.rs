b0490f050c8e80d2c94bbf3db6359551c1978df0
MarsMellow04 <louis.cutteridge@gmail.com>
2025-12-29 14:48:05 +0000

    Hold also done

--- cli/src/interactive/states/hold_sm/confirm_hold.rs
+++ cli/src/interactive/states/hold_sm/confirm_hold.rs
@@ -1,44 +1,33 @@
-use diplomacy::{Unit,UnitPosition, UnitType};
-use diplomacy::geo::RegionKey;
-use diplomacy::order::{MainCommand, MoveCommand};
-
 use crate::interactive::state_machine::{InputResult, MachineData, State};
-use crate::interactive::state_machine::StateMachine;
 use crate::interactive::states::show_units::ShowUnitState;
-use crate::interactive::states::terminal_state::TerminalState;
-use crate::rules::order_builder::OrderBuilder;
+use crate::interactive::util::{SelectResult, finalize_order, select_from};
+use crate::interactive::state_machine::UiState;
 
 #[derive(Clone, PartialEq)]
 pub struct ConfirmHold;
 
 impl State for ConfirmHold {
-    fn render(&self,  _machine: &StateMachine) {
-        println!("\nConfirm this move command 'y', or 'q' to quit, 'b' to go back");
-    }
+    fn render(&self,  _machine: &MachineData) {}
 
-    fn handle_input(&mut self, input: &str, _machine_data: &mut MachineData) -> Option<InputResult> {
-        match input.trim() {
-            "y" => return Some(InputResult::Advance),
-            "q" => return Some(InputResult::Quit),
-            "b" => return Some(InputResult::Continue), 
-            _ => {return Some(InputResult::Continue)}
+    fn handle_input(&mut self, _input: &str, machine_data: &mut MachineData, _ctx: &crate::rules::game_context::GameContext) -> InputResult {
+        let options = vec!["Yes", "No"];
+        match select_from("Do you confirm this order?", &options) {
+            SelectResult::Selected("Yes") => {
+                finalize_order(machine_data);
+                InputResult::Advance
+            }
+            SelectResult::Selected(_) => {
+                println!("Ok! Moving you back... ");
+                InputResult::Back}
+
+            SelectResult::Back => {InputResult::Back}
+            SelectResult::Quit => {InputResult::Quit}
         }
     }
 
-    fn next(self: Box<Self>, machine: &mut StateMachine) -> Box<dyn State> {
-        let unit = machine.data.selected_unit.as_ref().unwrap().clone();
 
-        // I want the parsing for orders to occur now
-        let order = OrderBuilder::new()
-            .nation("Fra")
-            .for_unit(&unit)
-            .hold()
-            .build();
-        machine.data.orders.push(order.unwrap());
-        println!("{:?}", machine.data.orders);
-        let index = machine.data.units_remaining.iter().position(|x| *x == unit).unwrap();
-        let remaining_units = machine.data.units_remaining.remove(index);
-        Box::new(ShowUnitState::new())
+    fn next(self, _machine_data: &mut MachineData) -> crate::interactive::state_machine::UiState {
+        UiState::ShowUnit(ShowUnitState) 
     }
 
     fn is_terminal(&self) -> bool {
